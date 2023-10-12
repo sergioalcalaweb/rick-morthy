de198dd755115baa886be7eb0ccbc506fc4bcfd3
Sergio Alcala <checo_2k2@hotmail.com>
2023-10-12 15:27:09 -0400

    hotfix add redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/1',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
